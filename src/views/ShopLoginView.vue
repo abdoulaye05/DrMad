c0e74691ac22a1f45aa07344469190d7f7462d56
amulle22 <arthur.muller@edu.univ-fcomte.fr>
2024-12-18 10:23:59 +0100

    AM - shoploginView

--- src/views/ShopLoginView.vue
+++ src/views/ShopLoginView.vue
@@ -6,7 +6,7 @@
         <v-form>
           <v-text-field
               v-model="login"
-              label="Login"
+              label="abdou le goat"
               outlined
               dense
           ></v-text-field>
